19680fdedd9f79f01aedaf38d27bd0913368b965
gaboohd <cabreraleonel973@gmail.com>
2024-06-12 18:31:06 -0300

    Agregar archivos de proyecto.

---
+++ semana4/ViewModels/LoginVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace semana4.ViewModels;
+
+public class LoginVM
+{
+	[Required(ErrorMessage = "Se requiere un Usuario.")]
+	public string? Usuario { get; set; }
+
+	[Required(ErrorMessage = "Se requiere una Contraseña.")]
+	[DataType(DataType.Password)]
+	public string? Contraseña { get; set; }
+
+	[Display(Name = "Hacerme acordar")]
+	public bool RememberMe { get; set; }
+}
---
+++ semana4/ViewModels/RegisterVM.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace semana4.ViewModels;
+
+public class RegisterVM
+{
+	[Required]
+	public string? Nombre { get; set; }
+	[Required]
+	[DataType(DataType.EmailAddress)]
+	public string? Email { get; set; }
+	[Required]
+	[DataType(DataType.Password)]
+	public string? Contraseña { get; set; }
+
+	[Compare("Contraseña", ErrorMessage = "Contraseña.")]
+	[Display(Name = "Confirma Contraseña")]
+	[DataType(DataType.Password)]
+	public string? Confirmar_Contraseña { get; set; }
+
+	[DataType(DataType.MultilineText)]
+	public string? Address { get; set; }
+}
